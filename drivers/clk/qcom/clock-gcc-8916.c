6253f13543c11677327efd77edc9ca09cee791a8
Shoaib0597 <Shoaib0595@gmail.com>
2015-12-14 17:29:44 +0530

    UC and OC GPU
    Signed-off-by: Hriday Sharma <hridaysharma42@gmail.com>
    
    Conflicts:
    	arch/arm/boot/dts/qcom/msm8916-gpu.dtsi
    	drivers/clk/qcom/clock-gcc-8916.c

--- drivers/clk/qcom/clock-gcc-8916.c
+++ drivers/clk/qcom/clock-gcc-8916.c
@@ -574,7 +574,7 @@ static struct clk_freq_tbl ftbl_gcc_oxili_gfx3d_465_clk[] = {
 	F(  19200000,	      xo,   1,	  0,	0),
 	F(  50000000,  gpll0_aux,  16,	  0,	0),
 	F(  80000000,  gpll0_aux,  10,	  0,	0),
-	F( 100000000,  gpll0_aux,   8,	  0,	0),
+	F(  50000000,  gpll0_aux,   8,	  0,	0),
 	F( 160000000,  gpll0_aux,   5,	  0,	0),
 	F( 177780000,  gpll0_aux, 4.5,	  0,	0),
 	F( 200000000,  gpll0_aux,   4,	  0,	0),
@@ -583,6 +583,7 @@ static struct clk_freq_tbl ftbl_gcc_oxili_gfx3d_465_clk[] = {
 	F( 310000000,	   gpll2,   3,	  0,	0),
 	F( 400000000,  gpll0_aux,   2,	  0,	0),
 	F( 465000000,      gpll2,   2,	  0,	0),
+	F( 650000000,      gpll2,   2,	  0,	0),
 	F_END
 };
 
@@ -590,7 +591,7 @@ static struct clk_freq_tbl ftbl_gcc_oxili_gfx3d_clk[] = {
 	F(  19200000,	      xo,   1,	  0,	0),
 	F(  50000000,  gpll0_aux,  16,	  0,	0),
 	F(  80000000,  gpll0_aux,  10,	  0,	0),
-	F( 100000000,  gpll0_aux,   8,	  0,	0),
+	F(  50000000,  gpll0_aux,   8,	  0,	0),
 	F( 160000000,  gpll0_aux,   5,	  0,	0),
 	F( 177780000,  gpll0_aux, 4.5,	  0,	0),
 	F( 200000000,  gpll0_aux,   4,	  0,	0),
@@ -598,6 +599,7 @@ static struct clk_freq_tbl ftbl_gcc_oxili_gfx3d_clk[] = {
 	F( 294912000,	   gpll1,   3,	  0,	0),
 	F( 310000000,	   gpll2,   3,	  0,	0),
 	F( 400000000,  gpll0_aux,   2,	  0,	0),
+	F( 650000000,      gpll2,   2,    0,    0),
 	F_END
 };
 
